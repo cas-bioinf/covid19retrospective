82b25554ed56e61c714b9c9afec1e66f2cfd86f4
martinmodrak <cerny.m@gmail.com>
2020-05-07 14:26:12 +0200

    Preparing scaffold for brms predictors

--- R/hmm_simulator.R
+++ R/hmm_simulator.R
@@ -62,18 +62,29 @@ hmm_simulator <- function(N_patients, N_time, N_mid_states, use_noisy_states = F
   rates_prior_mean[rate_discharge] <- 0.1
 
 
-  rates_prior_alpha <- rep(3, N_rates)
-  rates_prior_beta <- rates_prior_alpha / rates_prior_mean
+  # Predictors
+  N_predictor_sets <- 2
+  N <- N_predictor_sets * N_rates
+  rate_predictors <- array(1:(N_rates * N_predictor_sets), c(N_predictor_sets,N_rates))
 
-  rates <- rgamma(N_rates, rates_prior_alpha, rates_prior_beta)
-
-  rate_matrix <- matrix(0, nrow = N_states_hidden, ncol = N_states_hidden)
-  for(r in 1:N_rates) {
-    rate_matrix[rates_from[r], rates_to[r]] <- rates[r]
-    rate_matrix[rates_from[r], rates_from[r]] <- rate_matrix[rates_from[r], rates_from[r]] - rates[r]
+  intercept_prior_logmean <- array(NA_real_, N)
+  for(ps in 1:N_predictor_sets) {
+    intercept_prior_logmean[rate_predictors[ps,]] <- log(rates_prior_mean)
   }
+  intercept_prior_sd <- array(1, N);
+  mu <- rnorm(N, intercept_prior_logmean, intercept_prior_sd)
 
-  transition_matrix <- expm::expm(rate_matrix)
+  transition_matrices <- array(NA_real_, c(N_predictor_sets,N_states_hidden, ncol = N_states_hidden))
+
+  for(ps in 1:N_predictor_sets) {
+    rate_matrix <- matrix(0, nrow = N_states_hidden, ncol = N_states_hidden)
+    for(r in 1:N_rates) {
+      rate_value <- exp( mu[ rate_predictors[ps, r] ] )
+      rate_matrix[rates_from[r], rates_to[r]] <- rate_value
+      rate_matrix[rates_from[r], rates_from[r]] <- rate_matrix[rates_from[r], rates_from[r]] - rate_value
+    }
+    transition_matrices[ps, ,] <- expm::expm(rate_matrix)
+  }
 
 
   # Observation model
@@ -90,12 +101,21 @@ hmm_simulator <- function(N_patients, N_time, N_mid_states, use_noisy_states = F
     noisy_state_id <- array(NA_integer_, N_states_observed)
     noisy_state_id[noisy_states] <- 1:N_noisy_states
 
-    N_other_observations <- 2
-    noisy_states_other_obs <- array(NA_integer_, c(N_noisy_states, N_other_observations))
-    noisy_states_other_obs[1, ] <- c(noisy_states[1] + 1,noisy_states[1] + 2)
-    noisy_states_other_obs[N_noisy_states, ] <- c(noisy_states[N_noisy_states] - 2,noisy_states[N_noisy_states] - 1)
-    for(ns in 2:(N_noisy_states - 1)) {
-      noisy_states_other_obs[ns, ] <- c(noisy_states[ns] - 1, noisy_states[ns] + 1)
+    if(N_noisy_states == 1) {
+      stop("Need at least 2 mid states for noisy states")
+    } else if(N_noisy_states == 2) {
+      N_other_observations <- 1
+      noisy_states_other_obs <- array(NA_integer_, c(N_noisy_states, N_other_observations))
+      noisy_states_other_obs[1, ] <- noisy_states[2]
+      noisy_states_other_obs[2, ] <- noisy_states[1]
+    } else {
+      N_other_observations <- 2
+      noisy_states_other_obs <- array(NA_integer_, c(N_noisy_states, N_other_observations))
+      noisy_states_other_obs[1, ] <- c(noisy_states[1] + 1,noisy_states[1] + 2)
+      noisy_states_other_obs[N_noisy_states, ] <- c(noisy_states[N_noisy_states] - 2,noisy_states[N_noisy_states] - 1)
+      for(ns in 2:(N_noisy_states - 1)) {
+        noisy_states_other_obs[ns, ] <- c(noisy_states[ns] - 1, noisy_states[ns] + 1)
+      }
     }
 
     other_observations_probs <- array(NA_real_, c(N_noisy_states, N_other_observations))
@@ -113,48 +133,75 @@ hmm_simulator <- function(N_patients, N_time, N_mid_states, use_noisy_states = F
     other_observations_probs <- array(0, c(0, 1))
   }
 
+
+
   # The actual Markov chain
+  N_observations <- N_patients * N_time - floor(N_patients / 2)
+  patients <- array(NA_integer_, N_observations)
+  times <- array(NA_integer_, N_observations)
+  obs_states <- array(NA_integer_, N_observations)
+  predictor_sets <- array(NA_integer_, N_observations)
+
   initial_states_prob <- numeric(N_states_hidden)
   initial_states_prob[s_worsening_hid] <- 1 / N_mid_states
-  observations <- array(NA_integer_, c(N_time, N_patients))
-  true_base_states <- array(NA_integer_, c(N_time, N_patients))
-  true_improving <- array(FALSE, c(N_time, N_patients))
+
+  true_base_states <- array(NA_integer_, N_observations)
+  true_improving <- array(FALSE, N_observations)
+  next_observation <- 1
   for(p in 1:N_patients) {
+    if(p %% 2 == 1) {
+      max_time <- N_time
+    } else {
+      max_time <- N_time - 1
+    }
     state <- sample(1:N_states_hidden, 1, prob = initial_states_prob)
-    for(t in 1:N_time) {
+    for(t in 1:max_time) {
+      patients[next_observation] <- p
+      times[next_observation] <- t
+
       cor_obs <- corresponding_observation[state]
-      true_base_states[t, p] <- cor_obs
-      true_improving[t,p] <- state %in% s_improving_hid
+      true_base_states[next_observation] <- cor_obs
+      true_improving[next_observation] <- state %in% s_improving_hid
+
+      if(p %% 2 == 1 && t > 4) {
+        ps <- 1
+      } else {
+        ps <- 2
+      }
+      predictor_sets[next_observation] <- ps
 
       if(cor_obs %in% noisy_states) {
         noisy_id <- noisy_state_id[cor_obs]
         if(runif(1) < sensitivity[noisy_id]) {
-          observations[t, p] <- cor_obs
+          obs_states[next_observation] <- cor_obs
+        } else if(N_other_observations == 1) {
+          obs_states[next_observation] <- noisy_states_other_obs[noisy_id, 1]
         } else {
-          observations[t, p] <- sample(noisy_states_other_obs[noisy_id, ], size = 1,
+          obs_states[next_observation] <- sample(noisy_states_other_obs[noisy_id, ], size = 1,
                                        prob = other_observations_probs[noisy_id, ])
         }
       } else {
-        observations[t, p] <- cor_obs
+        obs_states[next_observation] <- cor_obs
       }
 
       #new state
-      state <- sample(1:N_states_hidden, size = 1, prob = transition_matrix[state, ])
+      state <- sample(1:N_states_hidden, size = 1, prob = transition_matrices[ps, state, ])
+      next_observation <- next_observation + 1
     }
   }
 
 list(
   observed = list(
-    N_patients = N_patients,
+    N = N,
+    intercept_prior_logmean = intercept_prior_logmean,
+    intercept_prior_sd = intercept_prior_sd,
+
     N_states_hidden = N_states_hidden,
     N_states_observed = N_states_observed,
-    N_time = N_time,
 
     N_rates = N_rates,
     rates_from = rates_from,
     rates_to = rates_to,
-    rates_prior_alpha = rates_prior_alpha,
-    rates_prior_beta = rates_prior_beta,
 
     corresponding_observation = corresponding_observation,
 
@@ -167,13 +214,23 @@ list(
 
     initial_states_prob = initial_states_prob,
 
-    observations = observations
+    N_observations = N_observations,
+    N_patients = N_patients,
+    N_time = N_time,
+    N_predictor_sets = N_predictor_sets,
+
+    patients = patients,
+    times = times,
+    obs_states = obs_states,
+
+    predictor_sets = predictor_sets,
+    rate_predictors = rate_predictors
   ),
   true = list(
-    rates = rates,
+    mu = mu,
     sensitivity = sensitivity,
     other_observations_probs = other_observations_probs,
-    transition_matrix = transition_matrix,
+    transition_matrices = transition_matrices,
     true_base_states = true_base_states,
     true_improving = true_improving
   )
--- hmm.stan
+++ hmm.stan
@@ -18,16 +18,18 @@ functions {
 }
 
 data {
-  int<lower=1> N_patients;
+  // This is to emulate brms
+  int N;
+  vector[N] intercept_prior_logmean;
+  vector<lower=0>[N] intercept_prior_sd;
+
+  // HMM data
   int<lower=1> N_states_hidden;
   int<lower=1> N_states_observed;
-  int<lower=1> N_time;
 
   int<lower=1> N_rates;
   int<lower=1, upper=N_states_hidden> rates_from[N_rates];
   int<lower=1, upper=N_states_hidden> rates_to[N_rates];
-  vector<lower=0>[N_rates] rates_prior_alpha;
-  vector<lower=0>[N_rates] rates_prior_beta;
 
   int<lower=1, upper=N_states_observed> corresponding_observation[N_states_hidden];
 
@@ -40,29 +42,75 @@ data {
 
   simplex[N_states_hidden] initial_states_prob;
 
-  int<lower=1, upper=N_states_observed> observations[N_time, N_patients];
+  // Observations
+  int<lower=1> N_observations;
+  int<lower=1> N_patients;
+  int<lower=1> N_time;
+  int<lower=1> N_predictor_sets;
+
+  int<lower=1, upper=N_patients> patients[N_observations];
+  int<lower=1, upper=N_time> times[N_observations];
+  //0 for unobserved states
+  int<lower=0, upper=N_states_observed> obs_states[N_observations];
+
+  int<lower=1, upper=N_predictor_sets> predictor_sets[N_observations];
+  int<lower=1, upper=N> rate_predictors[N_predictor_sets, N_rates];
 }
 
 transformed data {
   int<lower=0,upper=1> is_state_noisy[N_states_observed] = rep_array(0, N_states_observed);
   int<lower=0,upper=N_noisy_states> noisy_state_id[N_states_observed] = rep_array(0, N_states_observed);
 
+  //Rectangule observations and predictors. 0 for missing data
+  int<lower=0, upper=N_states_observed> obs_states_rect[N_patients, N_time] = rep_array(0, N_patients, N_time);
+  int<lower=0, upper=N_predictor_sets> predictor_sets_rect[N_patients, N_time] = rep_array(0, N_patients, N_time);
+  int<lower=0, upper=N_time> max_time[N_patients] = rep_array(0,N_patients);
+
   for(s_index in 1:N_noisy_states) {
     is_state_noisy[noisy_states[s_index]] = 1;
     noisy_state_id[noisy_states[s_index]] = s_index;
   }
+
+  for(o in 1:N_observations) {
+    obs_states_rect[patients[o], times[o]] = obs_states[o];
+    predictor_sets_rect[patients[o], times[o]] = predictor_sets[o];
+    if(obs_states[o] != 0) {
+      max_time[patients[o]] = max(max_time[patients[o]], times[o]);
+    }
+  }
+
+  //Check data validity
+  for(p in 1:N_patients) {
+    if(max_time[p] == 0) {
+      reject("Patient ", p, " has no osbservations");
+    }
+    for(t in 1:(max_time[p] - 1)) {
+      if(predictor_sets_rect[p, t] == 0) {
+        reject("Patient ", p, " has missing predictors for time ", t);
+      }
+    }
+  }
 }
 
 parameters {
-  vector<lower=0>[N_rates] rates;
   vector<lower=sensitivity_low_bound, upper=1>[N_noisy_states] sensitivity;
   simplex[N_other_observations] other_observations_probs[N_noisy_states];
+
+  //emulating brms
+  vector[N] mu;
 }
 
 model {
   matrix[N_states_hidden, N_states_observed] observation_probs = rep_matrix(0, N_states_hidden, N_states_observed);
-  matrix[N_states_hidden, N_states_hidden] transition_matrix_t =
-    transpose(compute_transition_matrix(N_states_hidden,  N_rates, rates_from, rates_to, rates));
+  matrix[N_states_hidden, N_states_hidden] transition_matrices_t[N_predictor_sets];
+  vector[N] exp_mu = exp(mu);
+
+  //Compute all transition matrices
+  for(ps in 1:N_predictor_sets) {
+    vector[N_rates] rate_values = exp_mu[rate_predictors[ps]];
+    transition_matrices_t[ps] =
+      transpose(compute_transition_matrix(N_states_hidden,  N_rates, rates_from, rates_to, rate_values));
+  }
 
   for(s_hidden in 1:N_states_hidden) {
     int corresponding_obs = corresponding_observation[s_hidden];
@@ -80,26 +128,27 @@ model {
   }
 
   for(p in 1:N_patients) {
-    matrix[N_states_hidden, N_time] alpha;
-    vector[N_time] alpha_log_norms;
-    alpha[, 1] = initial_states_prob .* observation_probs[, observations[1, p]];
+    matrix[N_states_hidden, max_time[p]] alpha;
+    vector[max_time[p]] alpha_log_norms;
+    alpha[, 1] = initial_states_prob .* observation_probs[, obs_states_rect[p, 1]];
 
     {
       real norm = max(alpha[, 1]);
       alpha[, 1] /= norm;
       alpha_log_norms[1] = log(norm);
     }
-    for(t in 2:N_time) {
-      real col_norm; //TODO merge with col_norm
-      alpha[, t] = observation_probs[, observations[t, p]] .* (transition_matrix_t * alpha[, t - 1]);
+    for(t in 2:max_time[p]) {
+      real col_norm;
+      int ps = predictor_sets_rect[p, t - 1];
+      alpha[, t] = observation_probs[, obs_states_rect[p, t]] .* (transition_matrices_t[ps] * alpha[, t - 1]);
       col_norm = max(alpha[, t]);
       alpha[, t] /= col_norm;
       alpha_log_norms[t] = log(col_norm) + alpha_log_norms[t - 1];
     }
-    target += log(sum(alpha[,N_time])) + alpha_log_norms[N_time];
+    target += log(sum(alpha[,max_time[p]])) + alpha_log_norms[max_time[p]];
   }
 
   // Priors
-  rates ~ gamma(rates_prior_alpha, rates_prior_beta);
+  mu ~ normal(intercept_prior_logmean, intercept_prior_sd);
 }
 
