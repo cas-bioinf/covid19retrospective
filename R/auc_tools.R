08287476c46907f66d22f7e9584c7e467978a514
martin_cerny <cerny.m@gmail.com>
2020-10-11 17:30:43 +0200

    Risk scores bascially complete (some polish remaining)

--- R/auc_tools.R
+++ R/auc_tools.R
@@ -7,14 +7,20 @@ outcome_caption = c("alive_12" = "12-day mortality", "alive_30" = "30-day mortal
                     "not_oxygen_admission" = "Not on oxygen on admission")
 score_caption = c("ACP_grade" = "Li et al. (ACP)",
                   "chen_liu" = "Chen & Liu",
-                  "chen_liu_incl" = "Chen & Liu",
                   "shi" = "Shi et al.",
                   "caramelo" = "Caramelo et al.",
-                  "bello_chavolla", "Bello-Chavolla et al.")
+                  "bc_base" = "Bello-Chavolla et al.",
+                  "bc0" = "Bello-Chavolla et al.",
+                  "bc1" = "Bello-Chavolla et al.",
+                  "caramelo_base" = "Caramelo et al.",
+                  "caramelo1" = "Caramelo et al.",
+                  "caramelo2" = "Caramelo et al.",
+                  "age" = "Age only"
+)
 
 outcome_note_caption = c("incl" = " (inclusion crit.)")
 
-my_auc <- function(f, d, outcome_note = "", subgroup = "") {
+my_auc_summary <- function(f, d, outcome_note = "", subgroup = "") {
   roc = pROC::roc(f, d, direction = "<", levels = c(TRUE, FALSE))
   ci = pROC::ci.auc(roc)
 
@@ -22,8 +28,7 @@ my_auc <- function(f, d, outcome_note = "", subgroup = "") {
   score = as.character(formula.tools::rhs(f))
 
 
-  list(roc = roc,
-       summary = tibble::tibble(
+  tibble::tibble(
          auc = ci[2],
          auc_low = ci[1],
          auc_high = ci[3],
@@ -31,79 +36,90 @@ my_auc <- function(f, d, outcome_note = "", subgroup = "") {
          score = score,
          outcome_note = outcome_note,
          subgroup = subgroup
-       )
+   )
+}
+
+my_auc_summary_from_estimate <- function(auc_estimate, outcome, score, outcome_note = "", subgroup = "") {
+  tibble::tibble(
+    auc = auc_estimate,
+    auc_low = NA_real_,
+    auc_high = NA_real_,
+    outcome = outcome,
+    score = score,
+    outcome_note = outcome_note,
+    subgroup = subgroup
   )
 }
 
-plot_my_auc <- function(auc_obj) {
-  if(!(auc_obj$summary$score %in% names(score_caption))) {
+
+plot_my_auc <- function(f, d, outcome_note = "") {
+
+  outcome = as.character(formula.tools::lhs(f))
+  score = as.character(formula.tools::rhs(f))
+
+  if(!(score %in% names(score_caption))) {
     stop("Score does not have caption")
   }
-  if(!(auc_obj$summary$outcome %in% names(outcome_caption))) {
+  if(!(outcome %in% names(outcome_caption))) {
     stop("Outcome does not have caption")
   }
 
-  if(auc_obj$summary$outcome_note != "") {
-    if(!(auc_obj$summary$outcome_note %in% names(outcome_note_caption))) {
+  if(outcome_note != "") {
+    if(!(outcome_note %in% names(outcome_note_caption))) {
       stop("Outcome note does not have caption")
     }
-    outcome_note_caption_value <- outcome_note_caption[auc_obj$summary$outcome_note]
+    outcome_note_caption_value <- outcome_note_caption[outcome_note]
   } else {
     outcome_note_caption_value <- ""
   }
 
 
-  caption = paste0(score_caption[auc_obj$summary$score], ", ", outcome_caption[auc_obj$summary$outcome],
+  caption = paste0(score_caption[score], ", ", outcome_caption[outcome],
                    outcome_note_caption_value)
-  plot(auc_obj$roc, auc.polygon=TRUE, max.auc.polygon=TRUE, print.auc = TRUE, main = caption)
+
+  roc = pROC::roc(f, d, direction = "<", levels = c(TRUE, FALSE))
+
+  plot(roc, auc.polygon=TRUE, max.auc.polygon=TRUE, print.auc = TRUE, main = caption)
 }
 
 plot_all_auc <- function(my_auc_list) {
   my_auc_list %>% walk(plot_my_auc)
 }
 
-my_auc_from_estimate <- function(auc_estimate, outcome, score, outcome_note, subgroup = "") {
-  list(roc = NULL,
-       summary = tibble::tibble(
-         auc = auc_estimate,
-         auc_low = NA_real_,
-         auc_high = NA_real_,
-         outcome = outcome,
-         score = score,
-         outcome_note = outcome_note,
-         subgroup = subgroup
-       )
-  )
 
-}
-
-compare_auc_to_orig <- function(orig_auc_list, our_auc_list, show_outcome = TRUE, ...) {
-  get_summaries <- function(x) {
-    map_df(x, ~ .x$summary)
-  }
+compare_auc_to_orig <- function(orig_auc, our_auc, show_outcome = TRUE, ...) {
 
   source_factor <- function(x) {
     factor(x, levels = c("our","orig"), labels = c("Present study", "Original study"))
   }
-  all_auc <- our_auc_list %>% map_df(get_summaries) %>%
+  all_auc <- our_auc %>%
     mutate(source = source_factor("our"), score = paste(score_caption[score], subgroup))
-  all_orig <- orig_auc_list %>% get_summaries() %>% mutate(source = source_factor("orig"), score = paste(score_caption[score], subgroup))
-  data_for_plot <- rbind(all_auc, all_orig) %>%
+
+  if(nrow(orig_auc) > 0) {
+    all_orig <- orig_auc %>% mutate(source = source_factor("orig"), score = paste(score_caption[score], subgroup))
+    data_for_plot_base <- rbind(all_auc, all_orig)
+  } else {
+    data_for_plot_base <- all_auc
+    all_orig <- all_auc %>% head(0) # Just to enforce correct columns
+  }
+  data_for_plot <- data_for_plot_base %>%
     mutate(
       id = interaction(outcome_note, outcome, source),
            size = if_else(source == "orig", 2, 1))
 
   res <- data_for_plot %>% ggplot(aes(x = auc, xmin = auc_low, xmax = auc_high, y = id, color = source, shape = source)) +
+    geom_vline(xintercept = 0.5, color = "darkblue", size = 2) +
     geom_vline(aes(xintercept = auc),data = all_orig, color = "gray", linetype = "dashed") +
     geom_errorbarh(height = 0, na.rm = TRUE) + geom_point(aes(size = size)) +
     scale_size(range = c(2,4), guide = FALSE) +
-    scale_x_continuous("C-statistic") +
+    scale_x_continuous("AUC") +
     scale_shape_discrete("Source") +
     scale_color_discrete("Source") +
-    facet_wrap(~score, scales = "free", ...)
+    facet_wrap(~score, scales = "free_y", ...) +
+    theme(axis.title.y = element_blank())
 
   if(!show_outcome) {
-    res <- res + theme(axis.text.y = element_blank(), axis.title.y = element_blank(), axis.ticks.y = element_blank())
+    res <- res + theme(axis.text.y = element_blank(), axis.ticks.y = element_blank())
   }
 
   res
